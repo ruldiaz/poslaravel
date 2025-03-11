627d20b8b60608fc8e270dd3fd0ae5f5f30e3dde
RaulDiaz <rulhdiaz@gmail.com>
2025-03-11 13:44:18 -0600

    admin profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -23,4 +24,11 @@ class AdminController extends Controller
     public function AdminLogoutPage() {
         return view('admin.admin_logout');
     }//end method
+
+    public function AdminProfile() {
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_profile_view', compact('adminData'));
+    }// end method
+
 }
