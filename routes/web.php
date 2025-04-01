65b5c97de0208eb81422b5b17381654ebe2de6f6
RaulDiaz <rulhdiaz@gmail.com>
2025-04-01 09:36:38 -0600

    suppliers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\EmployeeController;
 use App\Http\Controllers\Backend\CustomerController;
+use App\Http\Controllers\Backend\SupplierController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -54,5 +55,15 @@ Route::middleware(['auth'])->group(function(){
             Route::post('/update/customer','UpdateCustomer')->name('customer.update');
             Route::get('/delete/customer/{id}','DeleteCustomer')->name('delete.customer');
         });
+
+             // Supplier All Route
+             Route::controller(SupplierController::class)->group(function(){
+                Route::get('/all/supplier','AllSupplier')->name('all.supplier');
+                Route::get('/add/supplier','AddSupplier')->name('add.supplier');
+                Route::post('/store/supplier','StoreSupplier')->name('supplier.store');
+                Route::get('/edit/supplier/{id}','EditSupplier')->name('edit.supplier');
+                Route::post('/update/supplier','UpdateSupplier')->name('supplier.update');
+                Route::get('/delete/supplier/{id}','DeleteSupplier')->name('delete.supplier');
+            });
 }); // End user middleware
 
