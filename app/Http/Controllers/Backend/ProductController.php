e9a718a6f981eb923325e0e6d2e1efd89eb9c059
RaulDiaz <rulhdiaz@gmail.com>
2025-04-09 12:59:46 -0600

    product form

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
+use App\Models\Supplier;
 use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 
@@ -13,5 +15,11 @@ class ProductController extends Controller
     public function AllProduct() {
         $product = Product::latest()->get();
         return view('backend.product.all_product', compact('product'));
+    } // end method
+
+    public function AddProduct() {
+        $category = Category::latest()->get();
+        $supplier = Supplier::latest()->get();
+        return view('backend.product.add_product', compact('category','supplier'));
     } // end method
 }
--- routes/web.php
+++ routes/web.php
@@ -109,6 +109,7 @@ Route::middleware(['auth'])->group(function(){
         // Product All Route
         Route::controller(ProductController::class)->group(function(){
             Route::get('/all/product','AllProduct')->name('all.product');
+            Route::get('/add/product','AddProduct')->name('add.product');
             
         });
 }); // End user middleware
