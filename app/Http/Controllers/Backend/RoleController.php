7639196f74f8d21146e863a5fbeb9da90177f7d1
RaulDiaz <rulhdiaz@gmail.com>
2025-05-07 14:40:29 -0600

    edit and delete permission

--- app/Http/Controllers/Backend/RoleController.php
+++ app/Http/Controllers/Backend/RoleController.php
@@ -30,4 +30,34 @@ class RoleController extends Controller
         );
         return redirect()->route('all.permission')->with($notification);
     } // End method
+
+    public function EditPermission($id) {
+        $permission = Permission::findOrFail($id);
+        return view('backend.pages.permission.edit_permission', compact('permission'));
+    } // End mehod
+
+    public function UpdatePermission(Request $request) {
+        $per_id = $request->id;
+
+        Permission::findOrFail($per_id)->update([
+            'name' => $request->name,
+            'group_name' => $request->group_name,
+        ]);
+
+        $notification = array(
+            'message' => 'Permission Updated Succesfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.permission')->with($notification);
+    } // End method
+
+    public function DeletePermission($id) {
+        Permission::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Permission Deleted Succesfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    } // End method
 }
