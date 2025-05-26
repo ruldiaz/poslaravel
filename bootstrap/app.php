89222ac787e3567e9a118f4329e0427d206b7cdc
RaulDiaz <rulhdiaz@gmail.com>
2025-05-26 12:23:02 -0600

    middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+         // Registrar los middlewares de spatie aquí
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
+        
+        // Otros middlewares globales/pueden ir aquí si es necesario
+        // $middleware->append([]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -46,8 +46,8 @@ Route::middleware(['auth'])->group(function(){
 
     // Employee All Route
     Route::controller(EmployeeController::class)->group(function(){
-        Route::get('/all/employee','AllEmployee')->name('all.employee');
-        Route::get('/add/employee','AddEmployee')->name('add.employee');
+        Route::get('/all/employee','AllEmployee')->name('all.employee')->middleware('permission:employee.all');
+        Route::get('/add/employee','AddEmployee')->name('add.employee')->middleware('permission:employee.add');
         Route::post('/store/employee','StoreEmployee')->name('employee.store');
         Route::get('/edit/employee/{id}','EditEmployee')->name('edit.employee');
         Route::post('/update/employee','UpdateEmployee')->name('employee.update');
